6adbdc168cf85f320f1c27d50246db08d040c2ce
JimDeSmet <j773@telenet.be>
2019-06-22 23:40:22 +0200

    add basic query and mutation

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+             createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic cooking', 'All night coding', 'Sailing']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(4000, () => {
     console.log('server is listening');
 })
